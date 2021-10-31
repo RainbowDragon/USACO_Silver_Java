5c5baed89f005e44c8aba3971eb0b6a18a270801
RainbowDragon <xuanxiang@hotmail.com>
2021-10-30 21:50:44 -0700

    USACO 2017 - 01 - Problem 2 - Hoof, Paper, Scissors

--- src/2017-01/HoofPaperScissors.java
+++ src/2017-01/HoofPaperScissors.java
@@ -18,8 +18,8 @@ public class HoofPaperScissors {
 
         int n = Integer.parseInt(f.readLine());
         String[] hands = new String[n];
-        int[][] cdf = new int[n][3];
-        int[][] rcdf = new int[n][3];
+        int[][] prefixSum = new int[n][3];
+        int[][] suffixSum = new int[n][3];
 
         HashMap<String, Integer> map = new HashMap<String, Integer>();
         map.put("H", 0);
@@ -33,14 +33,14 @@ public class HoofPaperScissors {
             for (int j = 0; j < 3; j++)
             {
                 if (i > 0) {
-                    cdf[i][j] = cdf[i-1][j];
+                    prefixSum[i][j] = prefixSum[i-1][j];
                 }
                 else {
-                    cdf[i][j] = 0;
+                    prefixSum[i][j] = 0;
                 }
             }
 
-            cdf[i][map.get(hands[i])]++;
+            prefixSum[i][map.get(hands[i])]++;
         }
 
         for (int i = n-1; i >= 0; i--)
@@ -48,14 +48,14 @@ public class HoofPaperScissors {
             for (int j = 0; j < 3; j++)
             {
                 if (i < n-1) {
-                    rcdf[i][j] = rcdf[i+1][j];
+                    suffixSum[i][j] = suffixSum[i+1][j];
                 }
                 else {
-                    rcdf[i][j] = 0;
+                    suffixSum[i][j] = 0;
                 }
             }
 
-            rcdf[i][map.get(hands[i])]++;
+            suffixSum[i][map.get(hands[i])]++;
         }
 
         int result = 0;
@@ -66,7 +66,7 @@ public class HoofPaperScissors {
             {
                 for (int k = 0; k < 3; k++)
                 {
-                    int cur = cdf[i][j] + rcdf[i+1][k];
+                    int cur = prefixSum[i][j] + suffixSum[i+1][k];
                     if (cur > result)
                     {
                         result = cur;
