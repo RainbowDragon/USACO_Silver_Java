78b8dc1a8ce66049cfc8ea100918286976c21311
Xiang Xuan <xuanxiang@hotmail.com>
2022-01-06 23:56:11 -0800

    USACO 2021 - 12 - Problem 2 - Connecting Two Barns

--- src/2021-12/ConnectingTwoBarns.java
+++ src/2021-12/ConnectingTwoBarns.java
@@ -10,6 +10,8 @@ import java.util.*;
 public class ConnectingTwoBarns {
 
     static BufferedReader f;
+    static ArrayList<Integer>[] graph;
+    static int[] colors;
 
     public static void main (String [] args) throws IOException {
 
@@ -42,10 +44,12 @@ public class ConnectingTwoBarns {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[] states = new int[n];
+        graph = new ArrayList[n];
+        colors = new int[n];
         for (int i = 0; i < n; i++)
         {
-            states[i] = i;
+            graph[i] = new ArrayList<>();
+            colors[i] = -1;
         }
 
         for (int i = 0; i < m; i++)
@@ -53,129 +57,119 @@ public class ConnectingTwoBarns {
             st = new StringTokenizer(f.readLine());
             int start = Integer.parseInt(st.nextToken()) - 1;
             int end = Integer.parseInt(st.nextToken()) - 1;
-            union(states, start, end);
+            graph[start].add(end);
+            graph[end].add(start);
         }
 
-        for (int i = 0; i < n; i++)
-        {
-            states[i] = find(states, i);
-        }
+        dfs(0, 0);
 
-        int startState = states[0];
-        int endState = states[n-1];
-        if (startState == endState) {
+        if (colors[n-1] == 0) {
             return 0;
         }
 
-        ArrayList<Integer> starts = new ArrayList<>();
-        ArrayList<Integer> ends = new ArrayList<>();
-        ArrayList<Integer> middles = new ArrayList<>();
-
+        dfs(n-1, 1);
+        int c = 2;
         for (int i = 0; i < n; i++)
         {
-            if (states[i] == startState) {
-                starts.add(i);
-            }
-            else if (states[i] == endState) {
-                ends.add(i);
-            }
-            else {
-                middles.add(i);
+            if (colors[i] == -1) {
+                dfs(i, c);
+                c++;
             }
         }
 
-        long result = getShortestDistance(starts, ends);
-
-        HashMap<Integer, Long> startDistance = new HashMap<>();
-        HashMap<Integer, Long> endDistance = new HashMap<>();
-
-        getEndDistance(starts, middles, states, startDistance);
-        getEndDistance(ends, middles, states, endDistance);
-
-        for (Integer key : startDistance.keySet())
+        long[] d2s = new long[n];
+        for (int i = 0; i < n; i++)
         {
-            if (key != startState && key != endState) {
-                result = Math.min(result, startDistance.get(key)+endDistance.get(key));
+            d2s[i] = Long.MAX_VALUE;
+        }
+        int last = 200001;
+        for (int i = 0; i < n; i++)
+        {
+            if (colors[i] == 0) {
+                last = i;
             }
+
+            d2s[i] = Math.min(d2s[i], distance(i, last));
+        }
+        last = -100001;
+        for (int i = n-1; i >= 0; i--)
+        {
+            if (colors[i] == 0) {
+                last = i;
+            }
+
+            d2s[i] = Math.min(d2s[i], distance(i, last));
+        }
+
+        long[] d2e = new long[n];
+        for (int i = 0; i < n; i++)
+        {
+            d2e[i] = Long.MAX_VALUE;
+        }
+        last = 200001;
+        for (int i = 0; i < n; i++)
+        {
+            if (colors[i] == 1) {
+                last = i;
+            }
+
+            d2e[i] = Math.min(d2e[i], distance(i, last));
+        }
+        last = -100001;
+        for (int i = n-1; i >= 0; i--)
+        {
+            if (colors[i] == 1) {
+                last = i;
+            }
+
+            d2e[i] = Math.min(d2e[i], distance(i, last));
+        }
+
+        long[] c2s = new long[c];
+        for (int i = 0; i < c; i++)
+        {
+            c2s[i] = Long.MAX_VALUE;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            c2s[colors[i]] = Math.min(c2s[colors[i]], d2s[i]);
+        }
+
+        long[] c2e = new long[c];
+        for (int i = 0; i < c; i++)
+        {
+            c2e[i] = Long.MAX_VALUE;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            c2e[colors[i]] = Math.min(c2e[colors[i]], d2e[i]);
+        }
+
+        long result = Long.MAX_VALUE;
+        for (int i = 0; i < c; i++)
+        {
+            result = Math.min(result, c2s[i]+c2e[i]);
         }
 
         return result;
     }
 
-    public static int find(int[] states, int index) {
+    public static void dfs(int node, int color) {
 
-        while (index != states[index])
-        {
-            index = states[index];
+        if (colors[node] != -1) {
+            return;
         }
 
-        return index;
-    }
-
-    public static void union(int[] states, int index1, int index2) {
-
-        int root1 = find(states, index1);
-        int root2 = find(states, index2);
-
-        if (root1 != root2) {
-            states[root1] = states[root2] = Math.min(root1, root2);
-        }
-    }
-
-    public static long getShortestDistance(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-
-        long result = distance(list1.get(0), list2.get(0));
-        int p1 = 0;
-        int p2 = 0;
-
-        while (p1 < list1.size() && p2 < list2.size())
+        colors[node] = color;
+        for (int i : graph[node])
         {
-            long current = distance(list1.get(p1), list2.get(p2));
-            result = Math.min(result, current);
-
-            if (list1.get(p1) < list2.get(p2)) {
-                p1++;
-            }
-            else {
-                p2++;
-            }
-        }
-
-        return result;
-    }
-
-    public static void getEndDistance(ArrayList<Integer> oneEnd, ArrayList<Integer> middle, int[] states, HashMap<Integer, Long> endDistance) {
-
-        int index = 0;
-        for (int i = 0; i < middle.size(); i++)
-        {
-            while (index < oneEnd.size() && oneEnd.get(index) < middle.get(i))
-            {
-                index++;
-            }
-
-            if (index == oneEnd.size()) {
-                index--;
-            }
-
-            long current = distance(oneEnd.get(index), middle.get(i));
-            if (index > 0) {
-                current = Math.min(current, distance(oneEnd.get(index-1), middle.get(i)));
-            }
-
-            int key = states[middle.get(i)];
-            if (endDistance.containsKey(key)) {
-                endDistance.put(key, Math.min(endDistance.get(key), current));
-            }
-            else {
-                endDistance.put(key, current);
-            }
+            dfs(i, color);
         }
     }
 
     public static long distance(int a, int b) {
 
-        long result = a - b;
+        long result = (long)a - (long)b;
         return result * result;
     }
 }
